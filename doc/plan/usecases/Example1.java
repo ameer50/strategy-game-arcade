55691e309773897ca430294d4196705f2e96c830
Michael Xue <myx2@duke.edu>
2020-04-02 22:52:21 -0700

    Added Example5.java

--- doc/plan/usecases/Example1.java
+++ doc/plan/usecases/Example1.java
@@ -4,7 +4,7 @@ public class Controller{
 	
 	public void executeMovement(){
 		//using the getValidMoves method in the Board API we obtain all the possible moves for the piece at board cell 0, 0
-		List<String> moves = board.checkValidMoves(0, 0);
+		List<String> moves = board.getValidMoves(0, 0);
 		String move = moves.get(0);
 		//Using the doMove method in the Board API we execute the instruction stored in the first valid move sequence from the selected piece
 		double score = board.doMove(0, 0, move);
