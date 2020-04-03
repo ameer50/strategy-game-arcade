e5d48d451720c57b5000ace8f1d74ab9df9d820e
Karthik Ramachandran <karthik.ramachandran@duke.edu>
2020-04-02 22:20:04 -0700

    added usecase implementation 1

---
+++ doc/usecases/Example2.java
@@ -0,0 +1,12 @@
+public class Controller{
+	//This example illustrates how our Controller would execute a single move selected from the potential valid moves a given piece could make
+	ChessBoard board;
+	
+	public void executeMovement(){
+		//using the getValidMoves API we obtain all the possible moves for the piece at board cell 0, 0
+		List<String> moves = board.checkValidMoves(0, 0);
+		String move = moves.get(0);
+		//Using the doMove api we execute the instruction stored in the first valid move sequence from the selected piece 
+		board.doMove(0, 0, move);
+	}
+}
