039c7ac6f5505a5b43fa13089c10f5a44789e7d7
amjad.syedibrahim <amjad.syedibrahim@duke.edu>
2020-04-07 22:23:49 -0500

    Started validMoves method in CheckersBoard

--- src/ooga/board/CheckersBoard.java
+++ src/ooga/board/CheckersBoard.java
@@ -3,10 +3,13 @@ package ooga.board;
 import javafx.util.Pair;
 
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class CheckersBoard extends Board {
+
+    public List<Point2D> validMoves;
 
     public CheckersBoard(Map<String, String> settings, Map<Point2D.Double, String> locs, Map<String, Pair<String, Double>> pieces){
         super(settings, locs, pieces);
@@ -21,6 +24,8 @@ public class CheckersBoard extends Board {
     public List<Point2D> getValidMoves(int x, int y) {
         Piece currPiece = getPieceAt(x,y);
         if(currPiece==null){return null;}
+        validMoves = new ArrayList<Point2D>();
+
 
 
 
@@ -30,6 +35,50 @@ public class CheckersBoard extends Board {
     @Override
     public double doMove(int startX, int startY, int endX, int endY) {
         return 0;
+    }
+
+    private List<Point2D> left(int x, int y, int dist){
+
+    }
+
+    public void helper(int x, int y){
+        Piece piece = getPieceAt(x,y);
+
+
+
+
+    }
+
+    public boolean goLeft(int x, int y, Piece currPiece){
+        if(!isValidCell(x-1, y-1) || !isValidCell(x-2, y-2)){
+            return false;
+        }
+        Piece temp = getPieceAt(x-1, y-1);
+
+        if(!(temp.getColor().equals(currPiece.getColor())) && (getPieceAt(x-2, y-2) == null)){
+            validMoves.add(new Point2D.Double(x-2, y-2));
+        }
+
+        return goLeft(x-2, y-2, currPiece);
+
+    }
+
+    public boolean goRight(int x, int y, Piece currPiece){
+        if(!isValidCell(x+1, y+1) || !isValidCell(x+2, y+2)){
+            return false;
+        }
+        Piece temp = getPieceAt(x+1, y+1);
+
+        if(!(temp.getColor().equals(currPiece.getColor())) && (getPieceAt(x+2, y+2) == null)){
+            validMoves.add(new Point2D.Double(x+2, y+2));
+        }
+
+        return goRight(x+2, y+2, currPiece);
+
+    }
+
+    public boolean isOppColor(int x, int y, String currPieceColor){
+        return currPieceColor.equals(getPieceAt(x,y).getColor());
     }
 
 }
