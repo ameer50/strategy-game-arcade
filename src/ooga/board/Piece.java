fd47c180fc851fa1614ec7e0cb8180ecba71e1ab
amjad.syedibrahim <amjad.syedibrahim@duke.edu>
2020-04-07 21:05:33 -0500

    Shoulda been in my branch amjad

--- src/ooga/board/Piece.java
+++ src/ooga/board/Piece.java
@@ -4,24 +4,31 @@ public class Piece {
   private String myName;
   private String myPattern;
   private double myScore;
+  private String myColor;
 
-  public Piece(String name, String pattern, double score){
-    myName = name;
-    myPattern = pattern;
-    myScore = score;
+  public Piece(String name, String pattern, double score, String color){
+    this.myName = name;
+    this.myPattern = pattern;
+    this.myScore = score;
+    this.myColor = color;
+
   }
 
   @Override
   public String toString(){
-    return myName;
+    return this.myName;
   }
 
   public String getMovePattern(){
-    return myPattern;
+    return this.myPattern;
   }
 
   public double getValue(){
-    return myScore;
+    return this.myScore;
+  }
+
+  public String getColor(){
+    return this.myColor;
   }
 
 }
