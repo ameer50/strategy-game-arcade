5b7e97af2eed2ebbf5eff35340e48b6b0b1ae1a6
Karthik Ramachandran <karthik.ramachandran@duke.edu>
2020-04-18 22:26:19 -0700

    resolved redo and broken kings. limited kings movements

--- src/ooga/controller/Controller.java
+++ src/ooga/controller/Controller.java
@@ -61,11 +61,11 @@ public class Controller {
 
     private void setUpMenu() {
         menuScreen = new MenuScreen(this.stage);
-        printMessageAndTime("Set up Menu Screen.");
+        //printMessageAndTime("Set up Menu Screen.");
         menuScreen.setButtonListener(e -> {
             setUpGameScreen(menuScreen.getGameChoice(), menuScreen.getFileChoice());
         });
-        printMessageAndTime("Set up listeners.");
+        //printMessageAndTime("Set up listeners.");
     }
 
     private void setUpGameScreen(String gameChoice, String fileChoice) {
@@ -74,7 +74,7 @@ public class Controller {
 
         processor = new XMLProcessor();
         processor.parse(gameXML);
-        printMessageAndTime("XML parsed.");
+        //printMessageAndTime("XML parsed.");
 
         //TODO: Change to reflection.
         switch (gameType) {
@@ -85,10 +85,10 @@ public class Controller {
             case CHECKERS:
                 board = new CheckersBoard(processor.getSettings(), processor.getInitialPieceLocations(),
                     processor.getMovePatterns());
-        } printMessageAndTime("Setup Board.");
+        } //printMessageAndTime("Setup Board.");
 
         gameScreen = new GameScreen(this.stage, board.getWidth(), board.getHeight(), processor.getInitialPieceLocations()); // ***
-        printMessageAndTime("Setup Game Screen.");
+        //printMessageAndTime("Setup Game Screen.");
 
         boardView = gameScreen.getBoardView();
         dashboardView = gameScreen.getDashboardView();
@@ -122,11 +122,11 @@ public class Controller {
         /* X and Y are the indices of the cell clicked to move FROM */
         boardView.setOnPieceClicked((int x, int y) -> {
             PieceView pieceView = boardView.getCellAt(x, y).getPiece();
-            System.out.println(pieceView.getColor()); // ***
+            //System.out.println(pieceView.getColor()); // ***
             if (pieceView.getColor().equals(activePlayer.getColor())) {
                 boardView.setSelectedLocation(x, y);
                 boardView.highlightValidMoves(board.getValidMoves(x, y));
-                System.out.println("Highlighted moves.");
+                //System.out.println("Highlighted moves.");
             }
         });
 
@@ -143,7 +143,7 @@ public class Controller {
             dashboardView.setUndoRedoButtonsDisabled(history.isUndoDisabled(), history.isRedoDisabled());
 
             toggleActivePlayer();
-            printMessageAndTime("Did user's move.");
+            //printMessageAndTime("Did user's move.");
 
             String winner = board.checkWon();
             if (winner != null) {
@@ -153,7 +153,7 @@ public class Controller {
 
             if (activePlayer.isCPU()) {
                 doCPUMove();
-                printMessageAndTime("Did CPU's move.");
+                //printMessageAndTime("Did CPU's move.");
             }
         });
 
@@ -178,13 +178,11 @@ public class Controller {
                 PieceView capturedPieceView = new PieceView(capturedPiece.getFullName());
                 boardView.getCellAt(capturedPieceLocation).setPiece(capturedPieceView);
             }
-            board.print();
         });
 
         dashboardView.setRedoMoveClicked((e) -> {
             Move prevMove = history.redo();
             historyList.add(prevMove);
-
             doMove(prevMove);
             dashboardView.setUndoRedoButtonsDisabled(history.isUndoDisabled(), history.isRedoDisabled());
             toggleActivePlayer();
